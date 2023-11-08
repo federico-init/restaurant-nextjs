fff7050584e9c82e52ba5b85f2578dc18ee5d3a1
federico-init <fefe.borrometi@gmail.com>
2023-11-08 17:17:13 +0100

    chore: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,14 @@
+import Link from "next/link";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between">
+      <Link href="/" className="text-xl md:text-3xl font-bold italic">
+        Just Order
+      </Link>
+      <p className="uppercase">© All rights reserved.</p>
+    </div>
+  );
 };
 
 export default Footer;
