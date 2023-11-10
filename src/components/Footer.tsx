da9fa96e6f552adfec7e4ff71786a9eee1f94d9c
federico-init <fefe.borrometi@gmail.com>
2023-11-10 16:03:18 +0100

    style: minor change in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between">
+    <div className="h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between uppercase">
       <Link href="/" className="text-xl md:text-3xl font-bold italic">
         Just Order
       </Link>
-      <p className="uppercase">© All rights reserved.</p>
+      <p>© All rights reserved.</p>
     </div>
   );
 };
