dbe01e009e9bdd29c91c7c9e2d1abe4aef845873
federico-init <fefe.borrometi@gmail.com>
2023-11-11 00:53:17 +0100

    fix: minor footer style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between uppercase">
+    <div className="h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-red-500 flex items-center justify-between uppercase">
       <Link href="/" className="text-xl md:text-3xl font-bold italic">
         Just Order
       </Link>
